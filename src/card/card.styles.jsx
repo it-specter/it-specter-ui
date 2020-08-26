a27c86d9e076a4716bf36d393c39ccca1d0ee72e
AnsonCodeLap <anson-argyris@outlook.de>
2020-08-26 09:10:34 +0200

    9

--- src/card/card.styles.jsx
+++ src/card/card.styles.jsx
@@ -5,10 +5,12 @@ const StyledCard = styled.div`
   transition: 0.3s;
   height: ${({ height }) => height + 'px'};
   width: 100%;
-  background-color: ${(props) => props.color ? props.color:props.theme.colors.secondary};
+  background-color: ${(props) =>
+    props.color ? props.color : props.theme.colors.secondary};
   color: ${(props) => props.theme.typograph.color};
   :hover {
-    box-shadow: 0 5px 5px -3px rgba(0,0,0,.2), 0 8px 10px 1px rgba(0,0,0,.14), 0 3px 14px 2px rgba(0,0,0,.12);
+    box-shadow: 0 5px 5px -3px rgba(0, 0, 0, 0.2),
+      0 8px 10px 1px rgba(0, 0, 0, 0.14), 0 3px 14px 2px rgba(0, 0, 0, 0.12);
   }
 `;
 
