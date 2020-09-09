22efc1520e42e032eaa312a0deef9f6ad9ec5cf3
AnsonCodeLap <anson-argyris@outlook.de>
2020-09-09 13:30:50 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ export { Form } from './components/Form/index';
 
 export { Card } from './components/Card/index';
 
-
+export { themeDefault } from './config/themes/themeDefault';
 export { default as Button } from './components/button';
 export { default as Pic } from './components/pic';
 export { default as Typography } from './components/Typography/index';
